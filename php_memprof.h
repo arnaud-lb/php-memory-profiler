2da4e1ab311f65c0f8fe3c48cafef85d8fe716f1
Arnaud Le Blanc <arnaud.lb@gmail.com>
2021-03-29 17:38:57 +0200

    3.0.0

--- php_memprof.h
+++ php_memprof.h
@@ -16,7 +16,7 @@
 #define PHP_MEMPROF_H
 
 #define MEMPROF_NAME "memprof"
-#define PHP_MEMPROF_VERSION "2.1.2"
+#define PHP_MEMPROF_VERSION "3.0.0"
 
 extern zend_module_entry memprof_module_entry;
 #define phpext_memprof_ptr &memprof_module_entry
