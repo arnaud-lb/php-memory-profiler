88b8b61aace3858c7b86894dfd600a04dba3a913
Arnaud Le Blanc <arnaud.lb@gmail.com>
2021-07-02 16:26:06 +0200

    Check against php 8.1

--- .github/workflows/test/build-php.sh
+++ .github/workflows/test/build-php.sh
@@ -15,9 +15,20 @@ PREFIX="$HOME/php"
 if ! [ -f "$HOME/build-cache/php/$PREFIX/bin/php" ]; then
     echo "PHP build is not cached"
 
-    wget https://secure.php.net/distributions/php-${PHP_VERSION}.tar.bz2
-    tar xjf php-${PHP_VERSION}.tar.bz2
-    cd php-${PHP_VERSION}
+    if [ -z "$PHP_TAG" ]; then
+        wget https://secure.php.net/distributions/php-${PHP_VERSION}.tar.bz2
+        tar xjf php-${PHP_VERSION}.tar.bz2
+        cd php-${PHP_VERSION}
+    else
+        git clone --depth 1 --branch "$PHP_TAG" https://github.com/php/php-src.git
+        cd php-src
+
+        sudo apt-get -y install re2c
+    fi
+
+    if ! [ -f "configure" ]; then
+        ./buildconf --force
+    fi
 
     PHP_BUILD_FLAGS="--prefix=$PREFIX --disable-all --enable-cli --enable-cgi --with-config-file-scan-dir=$PREFIX/etc/php --with-zlib --enable-debug"
 
