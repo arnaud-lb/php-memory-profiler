7c8e996ba57dd248e4584eca0ce29fb057887390
Gaël Reyrol <gael.reyrol@mention.com>
2023-11-29 09:14:48 +0100

    feat: format yaml files and add new matrix test entries

--- .github/workflows/test/build-xdebug.sh
+++ .github/workflows/test/build-xdebug.sh
@@ -5,7 +5,7 @@ set -e
 export PATH="$HOME/php/bin:$PATH"
 hash -r
 
-git clone --depth 1 --branch "3.1.2" "https://github.com/xdebug/xdebug.git"
+git clone --depth 1 --branch "3.2.2" "https://github.com/xdebug/xdebug.git"
 
 cd xdebug
 
