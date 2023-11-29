89a5736f9ee96af677f2e2870d8f41beba069068
Gaël Reyrol <gael.reyrol@mention.com>
2023-11-29 12:22:55 +0100

    Allow output_format to be defined from ini setting (#97)
    * feat: allow output_format to be defined from ini setting
    
    * feat: use modify handler to validate output_format ini setting
    
    * fix: add contents.file in package.xml

--- memprof.c
+++ memprof.c
@@ -866,7 +866,7 @@ static void memprof_zend_error_cb_dump(MEMPROF_ZEND_ERROR_CB_ARGS)
 
 	WITHOUT_MALLOC_TRACKING {
 		if (MEMPROF_G(output_format) == FORMAT_CALLGRIND) {
-			filename = generate_filename("callgrind");
+			filename = generate_filename(MEMPROF_OUTPUT_FORMAT_CALLGRIND);
 			stream = php_stream_open_wrapper_ex(filename, "w", 0, NULL, NULL);
 			if (stream != NULL) {
 				error = !dump_callgrind(stream);
@@ -875,7 +875,7 @@ static void memprof_zend_error_cb_dump(MEMPROF_ZEND_ERROR_CB_ARGS)
 				error = 1;
 			}
 		} else if (MEMPROF_G(output_format) == FORMAT_PPROF) {
-			filename = generate_filename("pprof");
+			filename = generate_filename(MEMPROF_OUTPUT_FORMAT_PPROF);
 			stream = php_stream_open_wrapper_ex(filename, "w", 0, NULL, NULL);
 			if (stream != NULL) {
 				error = !dump_pprof(stream);
@@ -1189,10 +1189,25 @@ ZEND_GET_MODULE(memprof)
 #	endif
 #endif
 
+static ZEND_INI_MH(onUpdateOutputFormat)
+{
+	if (zend_string_equals_literal_ci(new_value, MEMPROF_OUTPUT_FORMAT_CALLGRIND)) {
+		MEMPROF_G(output_format) = FORMAT_CALLGRIND;
+	} else if (zend_string_equals_literal_ci(new_value, MEMPROF_OUTPUT_FORMAT_PPROF)) {
+		MEMPROF_G(output_format) = FORMAT_PPROF;
+	} else {
+		zend_error_noreturn(E_ERROR, "Invalid memprof.output_format setting. Should be \"callgrind\" or \"pprof\"");
+		return FAILURE;
+	}
+
+	return SUCCESS;
+}
+
 /* {{{ PHP_INI_BEGIN
  */
 PHP_INI_BEGIN()
 	STD_PHP_INI_ENTRY("memprof.output_dir", MEMPROF_TEMP_DIR, PHP_INI_ALL, OnUpdateStringUnempty, output_dir, zend_memprof_globals, memprof_globals)
+	STD_PHP_INI_ENTRY("memprof.output_format", MEMPROF_OUTPUT_FORMAT_CALLGRIND, PHP_INI_ALL, onUpdateOutputFormat, output_format, zend_memprof_globals, memprof_globals)
 PHP_INI_END()
 /* }}} */
 
---
+++ tests/autodump-pprof.phpt
@@ -0,0 +1,52 @@
+--TEST--
+autodump
+--ENV--
+MEMPROF_PROFILE=dump_on_limit
+--FILE--
+<?php
+
+$dir = sys_get_temp_dir() . '/' . microtime(true);
+var_dump($dir);
+var_dump(mkdir($dir));
+var_dump(memprof_enabled_flags());
+
+$buf = str_repeat("a", 5<<20);
+
+register_shutdown_function(function () use (&$buf, $dir) {
+    $buf = "";
+    var_dump(scandir($dir));
+});
+
+ini_set("memprof.output_dir", $dir);
+ini_set("memprof.output_format", "pprof");
+ini_set("memory_limit", 15<<20);
+
+function f() {
+    $a = [];
+    for (;;) {
+        $a[] = str_repeat("a", 1<<20);
+    }
+}
+
+f();
+--EXPECTF--
+string(%d) "/%s"
+bool(true)
+array(3) {
+  ["enabled"]=>
+  bool(true)
+  ["native"]=>
+  bool(false)
+  ["dump_on_limit"]=>
+  bool(true)
+}
+
+Fatal error: Allowed memory size of 15728640 bytes exhausted%S (tried to allocate %d bytes) (memprof dumped to %smemprof.pprof%s) in %s on line%a
+array(3) {
+  [0]=>
+  string(1) "."
+  [1]=>
+  string(2) ".."
+  [2]=>
+  string(%d) "memprof.pprof.%s"
+}
